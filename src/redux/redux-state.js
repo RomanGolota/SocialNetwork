87a174219b6d43547ffd7acd6c8c57202f8b3f8b
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2022-08-29 09:53:12 +0300

    Added redux extention

--- src/redux/redux-state.js
+++ src/redux/redux-state.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware }  from "redux";
+import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose }  from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -16,7 +16,9 @@ const reducers = combineReducers(
     }
 );
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
+window.__store__ = store;
 
 window.store = store;
 export default store;
